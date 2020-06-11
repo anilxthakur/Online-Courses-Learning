3f0ef78dc29d8f162d6c9fab7b4ccfd4f7ede1da
anilxthakur <66789827+anilxthakur@users.noreply.github.com>
2020-06-11 20:32:22 +0530

    Update UsingTheGeoJSONAPI.py
    Update UsingTheGeoJSONAPI.py

--- Coursera/Using Python to Access Web Data/Week-6/Assignment/UsingTheGeoJSONAPI.py
+++ Coursera/Using Python to Access Web Data/Week-6/Assignment/UsingTheGeoJSONAPI.py
@@ -1,21 +1,41 @@
-import urllib.request, urllib.parse, urllib.error
 import json
+import ssl
+import urllib.request,urllib.parse, urllib.error
 
-#Api
-api = 'http://py4e-data.dr-chuck.net/geojson?'
 
-#Input data
-link = input('Enter location: ')
-link = api + urllib.parse.urlencode({'address':link})
-print('Retrieving', link)
 
-html = urllib.request.urlopen(link).read().decode()
-print('Retrieved', len(html), 'characters')
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-try:
-    js = json.loads(html)
+
+
+#Stroring the given parameters
+api_key = 42
+serviceurl = "http://py4e-data.dr-chuck.net/json?"
+# sample_address = "South Federal University"
+data_address = "Kyiv Polytechnical Institute"
+address_wanted = data_address
+
+#Setting the GET parameters on the URL
+parameters = {"address": address_wanted, "key":api_key}
+paramsurl = urllib.parse.urlencode(parameters)
+
+#Generating the complete URL. Printing it in order to check if it's correct.
+queryurl = serviceurl.strip() + paramsurl.strip()
+print("DATA URL: ", queryurl)
+
+#Obtaining and reading the data
+try :
+    data_read = urllib.request.urlopen(queryurl , context=ctx).read()
+    data = data_read.decode()
+    # Parsing the data and looking for the field we want.
+    jsondata = json.loads(data)
+    print(jsondata)
+    place_id = jsondata["results"][0]["place_id"]
+    print("PLACE ID: ", place_id)
 except:
-    js = None
-
-placeId = js['results'][0]['place_id']
-print('Place id', placeId)
+    print("Error.....")
+    print("-"*50)
+    print(data)
